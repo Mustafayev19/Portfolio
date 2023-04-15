35ebe8ef348338a1e80e4ea1f6fabb900f35a9eb
Mustafayev19 <azemustafayev19@gmail.com>
2023-04-15 20:05:39 +0400

    preparation for the project

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        mainFont: ['Manrope', 'sans-serif'],
+        secondFont: ['DynaPuff', 'cursive']
+      },
+      colors: {
+        'blue1': '#0b132bff',
+        'blue2': '#1c2541ff',
+        'blue3': '#3a506bff',
+        'green1': '#5bc0beff',
+        'green2': '#6fffe9ff',
+        'white1': '#fbfffe'
+      }
+
+
+
+    },
+  },
+  plugins: [
+
+  ],
+}
+
