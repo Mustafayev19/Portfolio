b329c7283795529116638395980de45c6462da91
Mustafayev19 <azemustafayev19@gmail.com>
2023-04-30 23:09:56 +0400

    validations

--- src/app/home/contact/contact.component.ts
+++ src/app/home/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'footer-contact',
@@ -7,9 +8,50 @@ import { Component } from '@angular/core';
 })
 export class ContactComponent {
   constructor() { }
-  public fullName!: string
+  public Name!: string
   public emailAdress!: string
   public mobileNumber!: number
   public emailSubject!: string
   public emailMessage!: string
+
+
+  log(name: any) {
+    console.log(name)
+  }
+
+
+  getErrorsMessages(state: any): string[] {
+    let name: string = state.name
+    let errors: string[] = []
+
+
+
+
+    if (state.errors) {
+      for (let error in state.errors) {
+        switch (error) {
+          case "required":
+            errors.push(`${name} is required `);
+            break
+          case "pattern":
+            if (state.name == "emailAdress") {
+              errors.push(`please use email format`)
+            } else if (state.name == "mobileNumber") {
+              errors.push("please use just numbers")
+            }
+            else {
+              errors.push(`only letters and space`);
+            }
+            break
+
+        }
+      }
+    }
+    return errors
+  }
+
+
+
+
+
 }
