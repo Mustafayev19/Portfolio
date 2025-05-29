ebb6c5f5caca9c1085bab547775208b314f7909e
Mustafayev19 <azemustafayev19@gmail.com>
2025-05-29 21:16:25 +0400

    .

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,11 +7,12 @@ import { ServicesComponent } from './pages/services/services.component';
 import { AboutComponent } from './pages/about-me/about-me.component';
 
 export const routes: Routes = [
+  { path: '', redirectTo: 'about', pathMatch: 'full' },
   { path: 'about', component: AboutComponent },
   { path: 'skills', component: SkillsComponent },
   { path: 'projects', component: ProjectsComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'resume', component: ResumeComponent },
   { path: 'services', component: ServicesComponent },
-  { path: '**', redirectTo: '' },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
